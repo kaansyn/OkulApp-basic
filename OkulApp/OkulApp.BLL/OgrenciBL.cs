93007ff589f64d55d60dd2c860ed2456a96fe50a
Kaan <115550337+kaansyn@users.noreply.github.com>
2023-12-24 13:24:18 +0300

    OkulApp-basic

---
+++ OkulApp/OkulApp.BLL/OgrenciBL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+using DAL;
+using OkulApp.MODEL;
+
+namespace OkulApp.BLL
+{
+    public class OgrenciBL
+    {
+        public bool OgrenciEkle(Ogrenci ogr)
+        {
+            SqlParameter[] p = {
+                             new SqlParameter("@Ad",ogr.Ad),
+                             new SqlParameter("@Soyad",ogr.Soyad),
+                             new SqlParameter("@Numara",ogr.Numara)
+                         };
+
+            Helper hlp = new Helper();
+            return hlp.ExecuteNonQuery("Insert into tblOgrenciler values(@Ad, @Soyad, @Numara)", p) > 0;
+
+        }
+    }
+}
---
+++ OkulApp/OkulAppSube1BIL/frmOgrKayit.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace OkulAppSube1BIL
+{
+    partial class frmOgrKayit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtNumara = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(307, 245);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 0;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(296, 95);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(100, 22);
+            this.txtAd.TabIndex = 1;
+            // 
+            // txtNumara
+            // 
+            this.txtNumara.Location = new System.Drawing.Point(296, 193);
+            this.txtNumara.Name = "txtNumara";
+            this.txtNumara.Size = new System.Drawing.Size(100, 22);
+            this.txtNumara.TabIndex = 2;
+            
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(296, 146);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(100, 22);
+            this.txtSoyad.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Numara";
+            // 
+            // frmOgrKayit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtNumara);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.btnKaydet);
+            this.Name = "frmOgrKayit";
+            this.Text = "frmOgrKayit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtNumara;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
