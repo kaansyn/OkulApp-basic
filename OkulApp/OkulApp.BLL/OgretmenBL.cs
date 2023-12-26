cd27bc810bae3799b83857195b5d0ffc4be5fc15
Kaan <115550337+kaansyn@users.noreply.github.com>
2023-12-26 10:46:16 +0300

    Öğrenci silme işlemi ekledim
    Öğrenci silme işlemi ve bu işlemi gerçekleştirmeden önce veritabanında ıd si girilen kaydın olup olmadığını kontrol etme işlemini ekledim

---
+++ OkulApp/OkulApp.BLL/OgretmenBL.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using DAL;
+using OkulApp.MODEL;
+
+namespace OkulApp.BLL
+{
+    public class OgretmenBL
+    {
+        public bool OgretmenEkle(Ogretmen ogretmen)
+        {
+            SqlParameter[] p = {
+                            new SqlParameter("@TC", ogretmen.TC),
+                            new SqlParameter("@Ad", ogretmen.Ad),
+                            new SqlParameter("@Soyad", ogretmen.Soyad),
+                };
+
+            Helper hlp = new Helper();
+            return hlp.ExecuteNonQuery("Insert into tblOgretmenler values(@TC, @Ad, @Soyad)", p) > 0;
+        }
+    }
+}
